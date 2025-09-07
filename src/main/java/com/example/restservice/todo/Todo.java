822f5e3139008163d64a34955fff3242dbfc9030
Gustavo Silva <gustavops1985@gmail.com>
2025-09-06 19:51:11 -0700

    fix import

--- src/main/java/com/example/restservice/todo/Todo.java
+++ src/main/java/com/example/restservice/todo/Todo.java
@@ -1,7 +1,7 @@
 package com.example.restservice.todo;
 
 import jakarta.persistence.Entity;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.Id;
 
 @Entity
 public class Todo {
